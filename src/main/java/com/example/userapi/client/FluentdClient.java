a8fa9203d705a459652fb6dcb279c144c5a1dc7a
Andrew <svt.andrey1631@gmail.com>
2024-06-06 15:40:09 +0300

    efk-stack

--- src/main/java/com/example/userapi/client/FluentdClient.java
+++ src/main/java/com/example/userapi/client/FluentdClient.java
@@ -9,10 +9,9 @@ import java.util.Map;
 public class FluentdClient {
 
     private static final String FLUENTD_HOST = "localhost";
-    private static final String FLUENTD_PORT = "24224";
+    private static final int FLUENTD_PORT = 24224;
     private static final String FLUENTD_TAG = "userapi";
-
-    private final FluentLogger logger = FluentLogger.getLogger(FLUENTD_TAG, FLUENTD_HOST, Integer.parseInt(FLUENTD_PORT));
+    private final FluentLogger logger = FluentLogger.getLogger(FLUENTD_TAG, FLUENTD_HOST, FLUENTD_PORT);
 
     public void send(String tag, Map<String, Object> data) {
         logger.log(tag, data);
